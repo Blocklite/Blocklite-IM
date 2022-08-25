e19052e8d7b0303a93872988da1d1f92387bd44c
Denis Lafontant <icebox827@gmail.com>
2022-08-24 22:49:38 -0400

    Create associations and validations for user and messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :linx, :phone, presence: true
+  validates :first_name, :last_name, presence: true, length: { minimum: 2, maximum: 30 }
+  validates :email, presence: true, uniqueness: { case_sensitive: true }
+  validates_format_of :email, with: /\A\S+@.+\.\S+\z/
+  validates :password, presence: true, uniqueness: { case_ewnsitive: true }, length: { minimum: 8 }
+
+  has_many :messages
 end
