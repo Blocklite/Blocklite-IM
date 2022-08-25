1201cc7f56e74ed74973ea20b30148927070ad5f
Denis Lafontant <icebox827@gmail.com>
2022-08-24 23:15:13 -0400

    Create controllers

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+	def index
+		@users = User.all
+	end
+
+	def show
+		@user = user.find(param[:id])
+	end
+
+  def create
+    @user = User.create(user_params)
+
+    if@user.save
+      redirect to root_parh
+    else
+      flash[:alert] = 'Please try again'
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:first_name, :last_name, :email, :password)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :linx, :phone, presence: true
   validates :first_name, :last_name, presence: true, length: { minimum: 2, maximum: 30 }
   validates :email, presence: true, uniqueness: { case_sensitive: true }
   validates_format_of :email, with: /\A\S+@.+\.\S+\z/
