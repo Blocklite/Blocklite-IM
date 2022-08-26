fc778e67bb303c81d7aa9a9310d30815183eab46
Denis Lafontant <icebox827@gmail.com>
2022-08-26 18:45:59 -0400

    Update design

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  post 'register', to: 'users#create'
+  get 'register', to: 'users#new'
+  post 'create', to: 'users#create'
   post 'message', to: 'messages#create'
 
   mount ActionCable.server, at: '/cable'
