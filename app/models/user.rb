d8edb3e2b8a13f50aa83bbe142d5bcbba2534f13
Denis Lafontant <icebox827@gmail.com>
2022-08-27 15:47:14 -0400

    Fix bugs in application

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   validates :first_name, :last_name, presence: true, length: { minimum: 2, maximum: 30 }
   validates :email, presence: true, uniqueness: { case_sensitive: true }
   validates_format_of :email, with: /\A\S+@.+\.\S+\z/
-  validates :password, presence: true, uniqueness: { case_sensitive: true }, length: { minimum: 8 }
+  validates :password, presence: true, length: { minimum: 8 }
 
   has_many :messages
 
