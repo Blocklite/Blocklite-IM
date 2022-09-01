4058a599b0ad1ca6df3b25efb19ccdb9a500e6b2
Denis Lafontant <icebox827@gmail.com>
2022-09-01 06:52:18 -0400

    Update login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
       flash[:notice] = 'User created successfully'
       session[:user_id] = @user.id
       session[:first_name] = @user.first_name
-      redirect to login_path
+      redirect_to root_path
     else
       flash[:error] = 'Please try again'
       render 'new'
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
 
   private
 
-  def user_params
-    params.require(:user).permit(:linx, :first_name, :last_name, :email, :password_digest, :gender, :birthdate)
-  end
+    def user_params
+      params.require(:user).permit(:linx, :first_name, :last_name, :phone, :email, :password, :gender, :birthdate)
+    end
 end
